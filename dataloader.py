6db765f83a747140de866e94904316fb4e323d49
gitumarkk <gitumarkk@gmail.com>
2019-08-02 03:40:42 -0400

    added dataloader

--- dataloader.py
+++ dataloader.py
@@ -10,10 +10,11 @@ import torchvision.transforms as transforms
 # Third Party
 from PIL import Image
 
+CACHE_ROOT = Path.cwd() / 'dataset' / 'cache'
 
 class UCF101Dataset(Dataset):
     def __init__(self, root, transform=None):
-        pass
+        CACHE_ROOT.mkdir(parents=True, exist_ok=True)
 
     def __len__(self):
         return len(self.keys)
@@ -23,3 +24,32 @@ class UCF101Dataset(Dataset):
         transformed_img = self.transform(img)
         img.close()
         return transformed_img
+
+    def __getitem__(self, idx):
+        values_list = list(self.values)
+        value = values_list[idx]
+        label = int(value.get('label')) - 1
+        video_path = value.get('video_path')
+        video_name = value.get('video_name')
+
+        frame_path = 'val-{video_name}-frame-{index}_%d.jpg'.format(video_name=video_name, index=idx)
+
+        subprocess.call([
+            'ffmpeg',
+            '-i',
+            str(video_path),
+            '-vf',
+            "select='eq(n\,{index})'".format(index=idx),
+            '-vsync',
+            '0',
+            frame_path
+            ],
+            # stdout=FNULL,
+            stderr=subprocess.DEVNULL
+        )
+            
+        return ()
+
+
+if __name__ == '__main__':
+    dataset = UCF101Dataset('')
